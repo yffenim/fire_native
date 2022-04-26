793f610da52052b38f415234950426467c376425
E <effyverse@gmail.com>
2022-04-26 14:29:13 -0400

    form and greeting UI for adding a new model

--- src/screens/UserScreen.jsx
+++ src/screens/UserScreen.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Center, Text, Button } from "native-base";
+import UserGreeting from "../containers/UserGreeting"
+import NewModelForm from "../containers/NewModelForm"
 // import { getRequest } from '../functions/UserApiRequests.jsx';
 import l from "../../helpers/consolelog.js";
 // import { jsonToCSV } from 'react-native-csv'
@@ -7,18 +9,34 @@ import l from "../../helpers/consolelog.js";
 
 
 export default function UserScreen({navigation}) {
+  const [formData, setData] = React.useState({});
+  const [errors, setErrors] = React.useState({});
 
-// You are currently tracking: Alertness, Appetite
-// Did you know you can track up to 3 Things?
-// Add New Model: ________ (SAVE) 
-// Notice: Now go check your Add Page!
+  const validate = () => {
+    if (formData.name === undefined) {
+      setErrors({ ...errors,
+        name: 'Title is required'
+      });
+      return false;
+    } else if (formData.name.length < 3) {
+      setErrors({ ...errors,
+        name: 'Title is too short'
+      });
+      return false;
+    }
+    return true;
+  };
 
+  const onSubmit = () => {
+    validate() ? l('Submitted') : l('Validation Failed');
+  };
 
   return (
     <Center>
-      <Button>Add New Model</Button>
-
+      <UserGreeting />
+      <NewModelForm />
     </Center>
-  );
+  )
 }
+
 
